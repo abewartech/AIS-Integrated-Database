65d004d328eb825af25f8572c43a5d2a7ccb7367
RoryM <rmeyer@csir.co.za>
2020-04-09 09:41:44 +0200

    fixed the try/except in filereader

--- build/db/db_init/3_cagg_init.sql
+++ build/db/db_init/3_cagg_init.sql
@@ -72,23 +72,24 @@ AS
    FROM ais.pos_reports
   GROUP BY pos_reports.mmsi, (time_bucket('1 day'::interval, pos_reports.event_time));
 
-  
-DROP VIEW IF EXISTS ports_visited_cagg CASCADE;
-CREATE VIEW ports_visited_cagg WITH
-(timescaledb.continuous, timescaledb.refresh_interval = '2h')
-AS
-SELECT 
-	mmsi,
-	last(port_name, event_time) as port_name,
-	time_bucket('6h', event_time) as day,
-	last(event_time, event_time) as first_time,
-	last(event_time, event_time) as last_time,
-	min(sog,event_time) as min_sog,
-	last(position) as ship_pos,
-	last(geom) as port_pos	
-FROM ais.pos_reports, geo.world_port_index
-WHERE 
-hourly_vessel_agg.position && ST_BUFFER(geo.world_port_index.geom, 0.2)
-GROUP BY mmsi, day; 
+
+--  -- Doesnt' Work... Hitting TSDB limitations
+-- DROP VIEW IF EXISTS ports_visited_cagg CASCADE;
+-- CREATE VIEW ports_visited_cagg WITH
+-- (timescaledb.continuous, timescaledb.refresh_interval = '2h')
+-- AS
+-- SELECT 
+-- 	mmsi,
+-- 	last(port_name, event_time) as port_name,
+-- 	time_bucket('6h', event_time) as day,
+-- 	last(event_time, event_time) as first_time,
+-- 	last(event_time, event_time) as last_time,
+-- 	min(sog,event_time) as min_sog,
+-- 	last(position) as ship_pos,
+-- 	last(geom) as port_pos	
+-- FROM ais.pos_reports, geo.world_port_index
+-- WHERE 
+-- hourly_vessel_agg.position && ST_BUFFER(geo.world_port_index.geom, 0.2)
+-- GROUP BY mmsi, day; 
 
 COMMIT;
