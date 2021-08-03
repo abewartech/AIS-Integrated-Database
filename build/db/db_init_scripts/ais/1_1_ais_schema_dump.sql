9e90107d618faa4a68c1d0afdf72e93499cbf705
Rory Meyer <rmeyer@csir.co.za>
2021-08-03 12:43:57 +0200

    fixing up the init scripts

--- build/db/db_init_scripts/ais/1_1_ais_schema_dump.sql
+++ build/db/db_init_scripts/ais/1_1_ais_schema_dump.sql
@@ -7,16 +7,16 @@
 
 -- Started on 2021-08-03 10:38:33 UTC
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+-- SET statement_timeout = 0;
+-- SET lock_timeout = 0;
+-- SET idle_in_transaction_session_timeout = 0;
+-- SET client_encoding = 'UTF8';
+-- SET standard_conforming_strings = on;
+-- -- SELECT pg_catalog.set_config('search_path', '', false);
+-- SET check_function_bodies = false;
+-- SET xmloption = content;
+-- SET client_min_messages = warning;
+-- SET row_security = off;
 
 --
 -- TOC entry 14 (class 2615 OID 16386)
