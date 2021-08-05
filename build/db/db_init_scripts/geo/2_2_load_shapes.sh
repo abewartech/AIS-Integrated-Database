9c9036ac5a1ff6de0ad4ef7249c876f8a1060aee
Rory Meyer <rmeyer@csir.co.za>
2021-08-05 12:46:36 +0200

    adding libs via new dockerfile

--- build/db/db_init_scripts/geo/2_2_load_shapes.sh
+++ build/db/db_init_scripts/geo/2_2_load_shapes.sh
@@ -2,28 +2,46 @@
 echo 'Downloading shapefiles from Marine Regions WFS...'
 mkdir -p /tmp/shapes
 
-echo '--- World EEZ v11'
-wget -nc "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_boundaries&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_eez.zip
-echo '--- World 24 NM'
-wget -nc "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_24nm&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_24nm.zip
-echo '--- World 12 NM'
-wget -nc "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_12nm&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_12nm.zip
-echo '--- World Internal Waters'
-wget -nc "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_internal_waters&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_eez.zip
-echo '--- World Archipelagic Waters'
-wget -nc "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_archipelagic_waters&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_eez.zip
-echo '--- World High Seas v1'
-wget -nc "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=high_seas&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_eez.zip
 
+if [ ! -f /tmp/shapes/world_eez.zip ]; then
+    echo '--- Downloading World EEZ v11...'
+    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_boundaries&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_eez.zip
+fi
+
+
+if [ ! -f /tmp/shapes/world_24nm.zip ]; then
+    echo '--- Downloading World 24 NM...'
+    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_24nm&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_24nm.zip
+fi
+if [ ! -f /tmp/shapes/world_12nm.zip ]; then
+    echo '--- Downloading World 12 NM...'
+    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_12nm&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_12nm.zip
+fi
+if [ ! -f /tmp/shapes/world_int_waters.zip ]; then
+    echo '--- Downloading World Internal Waters...'
+    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_internal_waters&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_int_waters.zip
+fi
+if [ ! -f /tmp/shapes/world_archi_waters.zip ]; then
+    echo '--- Downloading World Archipelagic Waters...'
+    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_archipelagic_waters&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_archi_waters.zip
+fi
+if [ ! -f /tmp/shapes/high_seas.zip ]; then
+    echo '--- Downloading World High Seas v1...'
+    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=high_seas&outputformat=SHAPE-ZIP" -O /tmp/shapes/high_seas.zip
+fi
 echo 'Downloading shapefiles from other places'
-echo '--- World Port Index'
-wget -nc "https://msi.nga.mil/api/publications/download?key=16694622/SFH00000/WPI_Shapefile.zip&type=view" -O /tmp/shapes/wpi.zip
-echo '--- DEFF SAMPAZ MPA List'
-wget -nc "https://sfiler.environment.gov.za:8443/ssf/s/readFile/folderEntry/40950/8afbc1c77a484088017a5d45cb4202e6/1624344206000/last/SAMPAZ_OR_2021_Q1.zip" -O /tmp/shapes/sampaz.zip
-echo '--- Admin Boundaries for Countries'
-wget -nc "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip" -O /tmp/shapes/country.zip
-
-
+if [ ! -f /tmp/shapes/wpi.zip ]; then
+    echo '--- Downloading World Port Index...'
+    wget -q "https://msi.nga.mil/api/publications/download?key=16694622/SFH00000/WPI_Shapefile.zip&type=view" -O /tmp/shapes/wpi.zip
+fi
+if [ ! -f /tmp/shapes/sampaz.zip ]; then
+    echo '--- Downloading DEFF SAMPAZ MPA List...'
+    wget -q "https://sfiler.environment.gov.za:8443/ssf/s/readFile/folderEntry/40950/8afbc1c77a484088017a5d45cb4202e6/1624344206000/last/SAMPAZ_OR_2021_Q1.zip" -O /tmp/shapes/sampaz.zip
+fi
+if [ ! -f /tmp/shapes/country.zip ]; then
+    echo '--- Downloading Admin Boundaries for Countries...'
+    wget -q "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip" -O /tmp/shapes/country.zip
+fi
 echo 'Downloads Complete' 
 
 echo 'Loading shapefiles into GEO schema...' 
