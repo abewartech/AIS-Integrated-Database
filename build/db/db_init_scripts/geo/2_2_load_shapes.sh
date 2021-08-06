2589ed58560a81c3c4342e69ca6aa7299b40cc54
Rory Meyer <rmeyer@csir.co.za>
2021-08-06 09:42:59 +0200

    fiddling with geo startup scripts

--- build/db/db_init_scripts/geo/2_2_load_shapes.sh
+++ build/db/db_init_scripts/geo/2_2_load_shapes.sh
@@ -11,7 +11,7 @@ else
 fi
 echo '--- Loading World EEZ v11'
 unzip /tmp/shapes/world_eez.zip -d /tmp/unzips/.
-shp2pgsql -I /tmp/unzips/eez_boundaries_v10.shp geo.world_eez | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
+shp2pgsql -I /tmp/unzips/eez.shp geo.world_eez | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
 rm -rf ../unzips/*
 
 ######################################################
