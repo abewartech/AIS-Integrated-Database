4c60f38a29696f5cf7d11b506e6038628dd8f02e
Rory Meyer <rmeyer@csir.co.za>
2021-08-13 14:03:58 +0200

    start.sh needs a little fixing

--- start.sh
+++ start.sh
@@ -9,63 +9,63 @@ function select_tables {
     if [ "${ADMIN}" == 'TRUE' ]; then
             table=admin
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${AIS}" == 'TRUE' ]; then
             table=ais
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${ALERTING}" == 'TRUE' ]; then
             table=alerting
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${API}" == 'TRUE' ]; then
             table=api
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${GEO}" == 'TRUE' ]; then
             table=geo
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${PAN}" == 'TRUE' ]; then
             table=pan
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${SAR}" == 'TRUE' ]; then
             table=sar
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${TESTING}" == 'TRUE' ]; then
             table=testing
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
     if [ "${VMS}" == 'TRUE' ]; then
             table=vms
             echo "--${table^^}"
-            cp -R build/db/db_init_scripts/${table} build/db/scripts_to_run/
+            cp -R build/db/db_init_scripts/${table}/* build/db/scripts_to_run/
             sleep 2;
     fi
 
