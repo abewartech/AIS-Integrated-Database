4fd00b23bfc0165f510d3769a06ed38d28a1fa82
Rory Meyer <rmeyer@csir.co.za>
2021-08-13 14:55:02 +0200

    start.sh needs a little fixing

--- build/db/db_init_scripts/admin/99_1_users.sql
+++ build/db/db_init_scripts/admin/99_1_users.sql
@@ -1,2 +1,2 @@
-CREATE USER ocims with encrypted password 'super_secret_password';
+CREATE USER ocims2 with encrypted password 'super_secret_password';
 grant all privileges on database ivt to ocims;
