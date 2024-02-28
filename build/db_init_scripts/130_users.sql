6a62b73bd34096c9c17e3302f7e7b2925b8f19a0
Rory <mrrgvm@gmail.com>
2024-02-28 12:05:12 -0500

    workingon API

--- build/db_init_scripts/130_users.sql
+++ build/db_init_scripts/130_users.sql
@@ -4,11 +4,8 @@
 --   write user: can only write to certain schemas
 --   API data reader: can only read from certain schemas
 
-CREATE ROLE readonly_api LOGIN; -- for API and untrusted users
-CREATE ROLE readwrite_ais LOGIN; -- for DB inserters 
-CREATE ROLE admin SUPERUSER LOGIN;
-CREATE ROLE api WITH PASSWORD 'Secure_API_Password' LOGIN;
+CREATE USER api WITH PASSWORD 'Secure_API_Password' LOGIN;
+GRANT USAGE ON SCHEMA postgis_ftw TO api;
+GRANT SELECT ON ALL TABLES IN SCHEMA postgis_ftw TO api;
+ALTER DEFAULT PRIVILEGES IN SCHEMA postgis_ftw GRANT SELECT ON TABLES TO api;
 
-GRANT USAGE, SELECT ON ALL TABLES IN SCHEMA postgis_ftw TO readonly_api;
-GRANT USAGE, SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA ais TO readwrite_ais;
-GRANT readonly_api TO api;
