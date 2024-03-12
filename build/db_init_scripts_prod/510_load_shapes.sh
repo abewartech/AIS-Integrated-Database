9fd69d4f1894f767932b61f61907582fba21aeb6
Jorge Mendes <jorgecunhamendes@gmail.com>
2024-03-12 14:19:49 +0000

    Docker compose and database initialization fixes

--- build/db_init_scripts_prod/510_load_shapes.sh
+++ build/db_init_scripts_prod/510_load_shapes.sh
@@ -1,122 +1,119 @@
 #!/bin/bash
-echo 'Downloading shapefiles from Marine Regions WFS...'
+
 mkdir -p /tmp/shapes/
 mkdir -p /tmp/unzips/
 rm -rf /tmp/unzips/*
 
-if [ ! -f /tmp/shapes/world_eez.zip ]; then
-    echo '--- Downloading World EEZ v11...'
-    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_eez.zip
-else
-    echo 'World EEZ file exists. Skip download.'
-fi
-echo '--- Loading World EEZ v11'
-unzip /tmp/shapes/world_eez.zip -d /tmp/unzips/.
-shp2pgsql -W Latin1 -I /tmp/unzips/eez.shp geo.world_eez | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
 
-######################################################
-if [ ! -f /tmp/shapes/world_24nm.zip ]; then
-    echo '--- Downloading World 24 NM...'
-    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_24nm&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_24nm.zip
-else
-    echo 'World 24 NM file exists. Skip download.'
-fi
-echo '--- Loading World 24 NM'
-unzip /tmp/shapes/world_24nm.zip -d /tmp/unzips/.
-shp2pgsql -W Latin1 -I /tmp/unzips/eez_24nm.shp geo.eez_24nm | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
+# fetch_geo title table zip shape url [referer]
+fetch_geo () {
+    local title="$1"
+    local table="$2"
+    local zip="$3"
+    local shape="$4"
+    local url="$5"
+    local wget_referer=
+    if [ -n "$6" ] ; then wget_referer="--referer $6" ; fi
+    if [ ! -f "/tmp/shapes/${zip}" ]; then
+        echo "--- Downloading ${title}..."
+        wget -q $wget_referer "${url}" -O "/tmp/shapes/${zip}"
+    else
+        echo "${title} file exists. Skip download."
+    fi
+    echo "--- Loading ${title}"
+    unzip "/tmp/shapes/${zip}" -d /tmp/unzips/
+    shp2pgsql -W Latin1 -I "/tmp/unzips/${shape}" "geo.${table}" | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
+    rm -rf /tmp/unzips/*
+}
 
-######################################################
-if [ ! -f /tmp/shapes/world_12nm.zip ]; then
-    echo '--- Downloading World 12 NM...'
-    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_12nm&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_12nm.zip
-else
-    echo 'World 12 NM file exists. Skip download.'
-fi
-echo '--- Loading World 12 NM'
-unzip /tmp/shapes/world_12nm.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/eez_12nm.shp geo.eez_12nm | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
 
-######################################################
-if [ ! -f /tmp/shapes/world_int_waters.zip ]; then
-    echo '--- Downloading World Internal Waters...'
-    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_internal_waters&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_int_waters.zip
-else
-    echo 'World Internal Waters file exists. Skip download.'
-fi
-echo '--- Loading World Internal Waters'
-unzip /tmp/shapes/world_int_waters.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/eez_internal_waters.shp geo.eez_internal_waters | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
+#
+# Marine Regions WFS
+#
+echo 'Downloading shapefiles from Marine Regions WFS...'
 
-######################################################
-if [ ! -f /tmp/shapes/world_archi_waters.zip ]; then
-    echo '--- Downloading World Archipelagic Waters...'
-    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=eez_archipelagic_waters&outputformat=SHAPE-ZIP" -O /tmp/shapes/world_archi_waters.zip
-else
-    echo 'World World Archipelagic Waters file exists. Skip download.'
-fi
-echo '--- Loading World Archipelagic Waters'
-unzip /tmp/shapes/world_archi_waters.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/eez_archipelagic_waters.shp geo.eez_archipelagic_waters | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
 
-######################################################
-if [ ! -f /tmp/shapes/high_seas.zip ]; then
-    echo '--- Downloading World High Seas v1...'
-    wget -q "https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=high_seas&outputformat=SHAPE-ZIP" -O /tmp/shapes/high_seas.zip
-else
-    echo 'High Seas file exists. Skip download.'
-fi
-echo '--- Loading World High Seas v1' 
-unzip /tmp/shapes/high_seas.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/high_seas.shp geo.oceans_world | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
+MARINE_REGIONS_SHAPE_ZIP='https://geo.vliz.be/geoserver/MarineRegions/wfs?service=WFS&version=1.0.0&request=GetFeature&outputformat=SHAPE-ZIP'
 
-######################################################
+
+fetch_geo \
+    'World 24 NM' \
+    'world_eez' \
+    'world_eez.zip' \
+    'eez.shp' \
+    "${MARINE_REGIONS_SHAPE_ZIP}&typeName=eez"
+
+
+fetch_geo \
+    'World EEZ v11' \
+    'eez_24nm' \
+    'world_24nm.zip' \
+    'eez_24nm.shp' \
+    "${MARINE_REGIONS_SHAPE_ZIP}&typeName=eez_24nm"
+
+fetch_geo \
+    'World 12 NM' \
+    'eez_12nm' \
+    'world_12nm.zip' \
+    'eez_12nm.shp' \
+    "${MARINE_REGIONS_SHAPE_ZIP}&typeName=eez_12nm"
+
+fetch_geo \
+    'World Internal Waters' \
+    'eez_internal_waters' \
+    'world_int_waters.zip' \
+    'eez_internal_waters.shp' \
+    "${MARINE_REGIONS_SHAPE_ZIP}&typeName=eez_internal_waters"
+
+fetch_geo \
+    'World Archipelagic Waters' \
+    'eez_archipelagic_waters' \
+    'world_archi_waters.zip' \
+    'eez_archipelagic_waters.shp' \
+    "${MARINE_REGIONS_SHAPE_ZIP}&typeName=eez_archipelagic_waters"
+
+fetch_geo \
+    'World High Seas v1' \
+    'oceans_world' \
+    'high_seas.zip' \
+    'high_seas.shp' \
+    "${MARINE_REGIONS_SHAPE_ZIP}&typeName=high_seas"
+
+
+
+#
+# Other places
+#
 echo 'Downloading shapefiles from other places'
-######################################################
-if [ ! -f /tmp/shapes/wpi.zip ]; then
-    echo '--- Downloading World Port Index...'
-    wget -q "https://msi.nga.mil/api/publications/download?key=16694622/SFH00000/WPI_Shapefile.zip&type=view" -O /tmp/shapes/wpi.zip
-else
-    echo 'WPI file exists. Skip download.'
-fi
-echo '--- Loading World Port Index'
-unzip /tmp/shapes/wpi.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/WPI.shp geo.world_port_index | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
 
-######################################################
-if [ ! -f /tmp/shapes/sampaz.zip ]; then
-    echo '--- Downloading DEFF SAMPAZ MPA List...'
-    wget -q "https://sfiler.environment.gov.za:8443/ssf/s/readFile/folderEntry/40950/8afbc1c77a484088017a5d45cb4202e6/1624344206000/last/SAMPAZ_OR_2021_Q1.zip" -O /tmp/shapes/sampaz.zip
-else
-    echo 'SAMPAZ file exists. Skip download.'
-fi
-echo '--- Loading DEFF SAMPAZ MPA List'
-unzip /tmp/shapes/sampaz.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/SAMPAZ_OR_2021_Q1.shp geo.sampaz | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
 
-######################################################
-if [ ! -f /tmp/shapes/country.zip ]; then
-    echo '--- Downloading Admin Boundaries for Countries...'
-    wget -q "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip" -O /tmp/shapes/country.zip
-else
-    echo 'Admin Boundaries file exists. Skip download.'
-fi
-echo '--- Loading Country Admin Boundaries'
-unzip /tmp/shapes/country.zip -d /tmp/unzips/
-shp2pgsql -W Latin1 -I /tmp/unzips/ne_50m_admin_0_countries.shp geo.admin_0_countriesd_eez | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-rm -rf /tmp/unzips/*
+fetch_geo \
+    'World Port Index' \
+    'world_port_index' \
+    'wpi.zip' \
+    'WPI.shp' \
+    'https://msi.nga.mil/api/publications/download?key=16694622/SFH00000/WPI_Shapefile.zip&type=view'
 
-######################################################
+fetch_geo \
+    'DEFF SAMPAZ MPA List' \
+    'sampaz' \
+    'sampaz.zip' \
+    'SAMPAZ_OR_2021_Q1.shp' \
+    'https://sfiler.environment.gov.za:8443/ssf/s/readFile/folderEntry/40950/8afbc1c77a484088017a5d45cb4202e6/1624344206000/last/SAMPAZ_OR_2021_Q1.zip'
+
+fetch_geo \
+    'Admin Boundaries for Countries' \
+    'admin_0_countriesd_eez' \
+    'country.zip' \
+    'ne_50m_admin_0_countries.shp' \
+    'https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip' \
+    'https://www.naturalearthdata.com/'
+
+
+
+#
+# The end
+#
 echo '==============================================='
 echo 'Downloads Complete and inserted into geo schema' 
 echo '==============================================='
-
-# shp2pgsql -I /tmp/World_EEZ_v10_20180221/eez_boundaries_v10.shp geo.world_eez | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
-# shp2pgsql -I /tmp/WPI_Shapefile/WPI.shp geo.world_port_index | psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
